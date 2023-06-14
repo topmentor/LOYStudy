ecb326001e8f2e35326ec75e2b5acc4ed5098190
Roi <mailtoksy@gmail.com>
2023-06-14 13:57:42 +0900

    5주차 보강자료

--- src/BaseBallGame.java
+++ src/BaseBallGame.java
@@ -12,23 +12,20 @@ public class BaseballGame {
     public int gameCount = 10;
     private String[] resultText = null;
 
-    public BaseballGame(){
+    public BaseballGame() {
         this.gameCount = 10;
         resultText = new String[gameCount];
         user_input = new Integer[gameCount][3];
     }
 
-    public BaseballGame(int count){
+    public BaseballGame(int count) {
         this.gameCount = count;
         resultText = new String[gameCount];
         user_input = new Integer[gameCount][3];
     }
 
 
-    public void start() {
-
-
-        Scanner sc = new Scanner(System.in);
+    public void genNumber() {
         int value = 0;
 
         // 랜덤 수 만들기
@@ -36,71 +33,76 @@ public class BaseballGame {
             value = (int) Math.floor(Math.random() * 10); // 0~9
 
             // 중복 처리
-            if(Arrays.asList(rand_num).indexOf(value) == -1 && value > 0){
+            if (Arrays.asList(rand_num).indexOf(value) == -1 && value > 0) {
                 rand_num[i] = value;
                 System.out.println(rand_num[i]);
-            }else{
+            } else {
                 i--;
             }
         }
-
-        for(int cnt = 0; cnt < gameCount ; cnt++) {
-            strike_num = 0;
-            ball_num = 0;
-
-            System.out.println(">> 1~9 숫자를 하나씩 입력해 주세요.");
-            for (int i = 0; i < 3; i++) {
-                value = sc.nextInt();
-
-                // 중복 처리
-                if(Arrays.asList(user_input[cnt]).indexOf(value) == -1 && value > 0){
-                    user_input[cnt][i] = value;
-                }else{
-                    System.out.println("중복된 값이 있거나 잘못된 값입니다. 다시 입력해 주세요");
-                    i--;
-                }
-            }
-
-            int match_index = -1;
-            int i = 0;
-            for (int user_num : user_input[cnt]) {
-                match_index = Arrays.asList(rand_num).indexOf(user_num);
-                if (match_index != -1) {
-//                    System.out.println(" >> " + match_index + "  - " + user_num);
-                    if (match_index == i) {
-                        strike_num++;
-                    } else {
-                        ball_num++;
-                    }
-                }
-                i++;
-            }
-
-            String result = "" + (cnt+1) + "회차 판정 -- " + strike_num + " 스트라이트 , " + ball_num + " 볼 ";
-            System.out.println(result);
-            resultText[cnt] = result;
-
-            if(strike_num == 3){
-                System.out.println("게임 클리어");
-                break;
-            }else if(cnt == gameCount - 1){
-                System.out.println("게임 클리어 실패");
-                
-            }
-        }
-
     }
 
-    public void printResult(){
-        System.out.println("입력한 모든 값 ------------------------------ ");
-        for(int i=0; i< gameCount ; i++){
-            if(user_input[i][0] > 0){
-                System.out.println("" + (i+1) + "회차 입력 값 : " + user_input[i][0] + " , "+ user_input[i][1] + " , " + user_input[i][2]);
+    public void inputUserNumber(int cnt) {
+        Scanner sc = new Scanner(System.in);
+        int value = 0;
+
+        System.out.println(">> 1~9 숫자를 하나씩 입력해 주세요.");
+        user_input[cnt] = new Integer[3];
+
+        for (int i = 0; i < 3; i++) {
+            value = sc.nextInt();
+
+            // 중복 처리
+            if (Arrays.asList(user_input[cnt]).indexOf(value) == -1 && value > 0) {
+                user_input[cnt][i] = value;
+            } else {
+                System.out.println("중복된 값이 있거나 잘못된 값입니다. 다시 입력해 주세요");
+                i--;
             }
         }
-        System.out.println("전체 결과 보기 ------------------------------ ");
-        for(String res : resultText){
-            if(res != null && !res.equals("")){
+    }
+
+    public int checkNumber(int cnt) {
+
+        int value = 0;
+
+        int strike_num = 0;
+        int ball_num = 0;
+
+        int match_index = -1;
+        int i = 0;
+        for (int user_num : user_input[cnt]) {
+            match_index = Arrays.asList(rand_num).indexOf(user_num);
+            if (match_index != -1) {
+//                    System.out.println(" >> " + match_index + "  - " + user_num);
+                if (match_index == i) {
+                    strike_num++;
+                } else {
+                    ball_num++;
+                }
+            }
+            i++;
+        }
+
+        String result = "" + (cnt + 1) + "회차 판정 -- " + strike_num + " 스트라이트 , " + ball_num + " 볼 ";
+        System.out.println(result);
+        resultText[cnt] = result;
+
+        return strike_num;
+    }
+
+    public void printResult() {
+        System.out.println("\n\n입력한 모든 값 ------------------------------ ");
+        for (int i = 0; i < gameCount; i++) {
+            if (user_input[i] != null && user_input[i][0] != null) {
+                System.out.println("" + (i + 1) + "회차 입력 값 : " + user_input[i][0] + " , " + user_input[i][1] + " , " + user_input[i][2]);
+            }else{
+                break;
+            }
+        }
+        System.out.println("\n전체 결과 보기 ------------------------------ ");
+        for (String res : resultText) {
+            if (res != null && !res.equals("")) {
                 System.out.println(res);
             }
         }
--- src/Game.java
+++ src/Game.java
@@ -5,8 +5,20 @@ public class Game {
         int gameCount = 3;
         BaseballGame myGame = new BaseballGame(gameCount);
 
-        myGame.start();
-        myGame.printResult();
+        myGame.genNumber();
+        for(int cnt = 0; cnt < gameCount ; cnt++) {
+            myGame.inputUserNumber(cnt);
+            int strike_num = myGame.checkNumber(cnt);
 
+            if (strike_num == 3) {
+                System.out.println("게임 클리어");
+                break;
+            } else if (cnt == gameCount - 1) {
+                System.out.println("게임 클리어 실패");
+
+            }
+        }
+        myGame.printResult();
     }
+
 }
